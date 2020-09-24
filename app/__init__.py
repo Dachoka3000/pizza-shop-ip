89608ae2ddb3865d7d023b14ed015d4b89227074
Bernice2001 <wangechibanim@gmail.com>
2020-09-24 15:20:43 +0300

    new files

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import config_optionsbootstrap = Bootstrap()
+from config import config_options
+bootstrap = Bootstrap()
 
 
-db = SQLAlchemy()def create_app(config_name):
+db = SQLAlchemy()
+def create_app(config_name):
     app = Flask(__name__)    # Creating the app configurations
     app.config.from_object(config_options[config_name])    # Initializing flask extensions
     bootstrap.init_app(app)
--- manage.py
+++ manage.py
@@ -4,16 +4,20 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand# Creating app instance
 
 
-app = create_app('development')manager = Manager(app)
-manager.add_command('server', Server)migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)@manager.command
+app = create_app('development')
+manager = Manager(app)
+manager.add_command('server', Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+3@manager.command
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)@manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)if __name__ == '__main__':
-    manager.run()
+    return dict(app=app, db=db, User=User, Role=Role)
+    if __name__ == '__main__':
+      manager.run()
 
 
