78872f70436f95baa3be0bdc2d57ff2a8a72c004
comfort museo <you@example.com>
2020-09-28 03:44:52 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import config_options
 
-from .main import main as main_blueprint
-from .auth import auth as auth_blueprint
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -18,6 +16,10 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
     db.init_app(app)
+
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+
 
     # Registering blueprint
     app.register_blueprint(main_blueprint)
