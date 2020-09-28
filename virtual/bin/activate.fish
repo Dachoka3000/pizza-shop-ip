bbc0bce323a9b694ee5dce76bde139b87f3e621e
comfort museo <you@example.com>
2020-09-28 04:57:01 +0300

    pullfromtest

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/daisy/Desktop/pizza-shop/virtual"
+set -gx VIRTUAL_ENV "/home/comfort/Desktop/pizza-shop-ip/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
